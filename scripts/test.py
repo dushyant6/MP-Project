072608661e35296420e1ce7c5e3bef1420ff4a13
Dushyant Patil <dushyantptl6@gmail.com>
2023-04-14 16:51:33 -0400

    Updated/ automated path update and plotting

--- scripts/test.py
+++ scripts/test.py
@@ -43,74 +43,40 @@ def plot_path_update(path_list):
 
 	return path_list
 
-	
-
-def path_list_for_plotting(path_list):
-	for path in path_list:
-		for i in range(len(path)):
-			path_x.append(path[i][0])
-			path_y.append(path[i][1])
-
-
 
 def main():
-	#---------------start of first path finding---------------------------
-	start = (5,5)
-	goal = (7,11)
-	sx, sy, gx, gy = [], [], [], []
-	path_list = []
-	sx.append(start[0]); sy.append(start[1])
-	gx.append(goal[0]); gy.append(goal[1])
+	start_pts = [(5,5), (7,5), (1,6), (15,22)]
+	goal_pts = [(7,11), (3,17), (8,17), (3,10)]
 	ox, oy = [], []
-	path = astar(start, goal, ox, oy)
-	timed_keys = time_a_star(path)
-
-	#---------------start of second path finding---------------------------
-	start = (7,5)
-	goal = (3,17)
-	sx.append(start[0]); sy.append(start[1])
-	gx.append(goal[0]); gy.append(goal[1])
-	path2 = astar(start, goal, ox, oy)
+	path_list = []
 	
-	path2 = update_path(timed_keys, path2)
-	
-	new_keys = update_time_dict(timed_keys, path2)
-	timed_keys2 = time_a_star(path2)
-	
-	print(f'length of path1 = {len(timed_keys)}, length of keys 2 = {len(timed_keys2)}, length of new keys = {len(new_keys)}')
-	
-	#---------------start of third path finding---------------------------
-	start = (1,6)
-	goal = (8,17)
-	sx.append(start[0]); sy.append(start[1])
-	gx.append(goal[0]); gy.append(goal[1])
-	path3 = astar(start, goal, ox, oy)
-
-	path3 = update_path(new_keys, path3)
-
-	timed_keys3 = time_a_star(path3)
-	print(f' max path length = {len(path), len(path2), len(path3)}')
-	path_list = [path, path2, path3]
-	path_all = plot_path_update(path_list)
-	print(f'length of path all = {len(path_all)}x{len(path_all[0])}x{len(path_all[0][1])}')
-	x_path_list, y_path_list = [], []
-	for seg in path_all:
-		for xy in seg:
-			x_path_list.append(xy[0])
-			y_path_list.append(xy[1])
+	for i in range(len(start_pts)):
+		#calculate shortest path
+		path = astar(start_pts[i], goal_pts[i], ox, oy)
+		if (i == 0):
+			#create a robot coordinate list with (x,y,t)
+			timed_keys = time_a_star(path)
+		else:
+			#update path to avoid collision with prev robots paths
+			path = update_path(timed_keys, path)
+			timed_keys = update_time_dict(timed_keys, path)
+		#create a path lists for plotting
+		path_list.append(path)
+		path_all = plot_path_update(path_list)	
 
 	#----------------------------------- start of plotting -----------------------------------------------/	
-	for i in range(len(path2)):
+	for i in range(len(path_all[0])):
 		plt.cla()
 		plt.plot(ox,oy,'sk')
-		plt.plot(sx,sy,'sg')
-		plt.plot(gx,gy,'sr',linewidth = 0.1)
-
+		for start, goal in zip(start_pts, goal_pts):
+			plt.plot(start[0], start[1],'sg')
+			plt.plot(goal[0], goal[1],'sr')
+			
 		for path_n in path_all:
 			draw_bot(path_n[i][0], path_n[i][1])
 		plt.axis('equal')
-		plt.xlim([-10,30])
-		plt.ylim([-10,30])
+		plt.xlim([0,30])
+		plt.ylim([0,30])
 		plt.show(block=False)	
 		plt.pause(0.5)
 		
