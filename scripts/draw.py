819ada2fbf9b9d949e2fb5d0fac8280626469b78
Dushyant Patil <dushyantptl6@gmail.com>
2023-04-15 23:36:08 -0400

    Add files via upload

--- scripts/draw.py
+++ scripts/draw.py
@@ -9,14 +9,17 @@ class C:
     W = 0.5
 
 
-#def draw_bot(x, y, color = "black"):
 def draw_bot(x, y):
-    car = np.array([[C.RB, -C.RB, -C.RF, C.RF, C.RB], 
-                    [C.W/2, C.W/2, -C.W/2, -C.W/2, C.W/2]])
+    car = np.array([[C.RB, -C.RB, -C.RF, C.RF, C.RB, -C.RB, -C.RB, C.RB], 
+                    [C.W/2, C.W/2, -C.W/2, -C.W/2, C.W/2, -C.W/2, C.W/2, -C.W/2]])
                    #[C.W/2, -C.W/2, -C.W/2, C.W/2, C.W/2]
     car += np.array([[x], [y]])
-
     plt.plot(car[0, :], car[1, :])
-    #plt.plot(car[0, :], car[1, :], color)
 
+
+def draw_endpoint(point, color= 'black'):
+	end = np.array([[C.RB+0.1, -C.RB-0.1, -C.RF-0.1, C.RF+0.1, C.RB+0.1], 
+		    [C.W/2+0.1, C.W/2+0.1, -C.W/2-0.1, -C.W/2-0.1, C.W/2+0.1]])
+	end += np.array([[point[0]], [point[1]]])
+	plt.plot(end[0, :], end[1, :], color)
 
